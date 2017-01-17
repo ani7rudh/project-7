618e5e89903add227d629dc6e39a95ee82632f3c
ani7rudh <yadavanirudh6@gmail.com>
2017-01-17 21:34:19 +0530

    first project 7
    first project 7

---
+++ TechFab/src/main/java/com/electro/dao/WishListDAO.java
@@ -0,0 +1,14 @@
+package com.electro.dao;
+
+import java.util.List;
+
+import com.electro.model.WishList;
+
+public interface WishListDAO {
+	
+	public void addWishList(WishList wishList);
+	public List<WishList> getWishList();
+
+
+
+}
---
+++ TechFab/src/main/webapp/WEB-INF/views/WishList.jsp
@@ -0,0 +1,9 @@
+ <%@ include file="header.jsp" %>
+
+  this is cartList
+  
+  
+  
+  <a href="checkout?userId=${sessionScope.userId}">click here for checkout</a>
+  
+   <%@ include file="footer.jsp" %>
